3dfc3db00a60f777061d036773ff677b9ca005e9
bassimatteo <bassimatteo88@gmail.com>
2015-11-11 18:20:15 +0100

    solved import order validation

--- rest-sample/src/main/java/hello/GreetingController.java
+++ rest-sample/src/main/java/hello/GreetingController.java
@@ -1,11 +1,11 @@
 package hello;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
@@ -13,6 +13,11 @@ public class GreetingController {
   private static final String template = "Hello, %s!";
   private final AtomicLong counter = new AtomicLong();
 
+  /**
+   * Test.
+   * 
+   * @return a string
+   */
   @RequestMapping(value = "/", method = RequestMethod.GET, produces = "text/plain")
   public String test() {
 
@@ -24,4 +29,5 @@ public class GreetingController {
   public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
     return new Greeting(counter.incrementAndGet(), String.format(template, name));
   }
+
 }
